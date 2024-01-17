3acc24c06fa14f1b3a976e8910c84fa41e047ab0
home <home@homes-MacBook-Pro.local>
2024-01-17 20:53:03 +0500

    macos cyrill

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,7 +3,32 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
+    
+    let channelName : String = "Printing"
+    
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+      
     return true
   }
+    
+    
+    override func applicationDidFinishLaunching(_ notification : Notification) {
+        let controller : FlutterViewController = mainFlutterWindow?.contentViewController as! FlutterViewController
+        let channel = FlutterMethodChannel.init(name: channelName, binaryMessenger: controller.engine.binaryMessenger)
+        channel.setMethodCallHandler({ (_ call: FlutterMethodCall, _ result: FlutterResult) -> Void in
+                    if ( call.method == "encode") {
+                        // Those values are guaranteed by the Dart code
+                        let args = call.arguments as! [String:Any]
+                        let data = args["data"] as! NSString
+                        let encode = String.Encoding(rawValue: CFStringConvertEncodingToNSStringEncoding(
+                            CFStringConvertIANACharSetNameToEncoding("CP866" as CFString)
+                        ))
+                        
+                        if let output = data.data(using: encode.rawValue) {
+                            result(FlutterStandardTypedData.init(bytes: output))
+                        }
+                    }
+                });
+    }
+    
 }
